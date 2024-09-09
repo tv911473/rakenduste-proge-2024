ff20b1cea327d07a2d69c17390a873629a196fc3
Taavi <144431054+tv911473@users.noreply.github.com>
2024-09-09 17:32:28 +0300

    feat: new components

--- 2024-09-09/first.js
+++ 2024-09-09/first.js
@@ -1,5 +1,5 @@
 // ylesanne 11: filtreeri valja suuremad kui 4
-const array = [1, 2, 3, 4, 5, 6]
+const array = [1, 2, 3, 4, 5, 6];
 
 const filteredArray = array.filter((element) => element > 4);
 
@@ -10,37 +10,37 @@ console.log({ filteredArray: filteredArray });
 // ylesanne 12:
 const names = ["Anni", "Mati", "Mari", "Juku"];
 
-const objectifiedNames = names.map(name => {
-    return {
-        name: name,
-        age: 24,
-        email: `${name}@company.com`,
-        address: `${name} Street 55`,
-        username: name.split("").reverse().join("")
-    };
-})
+const objectifiedNames = names.map((name) => {
+  return {
+    name: name,
+    age: 24,
+    email: `${name}@company.com`,
+    address: `${name} Street 55`,
+    username: name.split("").reverse().join(""),
+  };
+});
 
 console.log(objectifiedNames);
 
 // ylesanne 13: spread syntax
 let taavi = {
-    name: "Taavi",
-    school: "TLU"
-}
+  name: "Taavi",
+  school: "TLU",
+};
 
-taavi = {...taavi, height: 180};
+taavi = { ...taavi, height: 180 };
 
-console.log({taavi});
+console.log({ taavi });
 // 1 == "1" yhtlustab
 // 1 === "1" vordub
 
 // asunc/await Promise:
 const myPromise = () => {
-    return new Promise((resolve) => setTimeout(() => resolve("done"), 1000))
+  return new Promise((resolve) => setTimeout(() => resolve("done"), 1000));
 };
 
 const runPromise = async () => {
-    console.log(await myPromise()); // await ootab Promise 2ra
+  console.log(await myPromise()); // await ootab Promise 2ra
 };
 
 runPromise();
