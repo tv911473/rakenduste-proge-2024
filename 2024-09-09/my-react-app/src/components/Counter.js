b92e5a76330e5e8d3825271a3c3c92a0cb91bcb7
Taavi <144431054+tv911473@users.noreply.github.com>
2024-09-15 12:16:39 +0300

    feat: Counter.js buttons assigned to values

--- 2024-09-09/my-react-app/src/components/Counter.js
+++ 2024-09-09/my-react-app/src/components/Counter.js
@@ -4,19 +4,26 @@ const Counter = () => {
   const [counter, setCounter] = useState(0)
   // const [counter, setCounter] = React.useState(0);
 
-  const modifyCounter = () => setCounter(prevCounter => prevCounter + 1)
+  const modifyCounter = amount =>
+    setCounter(prevCounter => prevCounter + amount)
 
   return (
     <>
       <h1>{counter}</h1>
 
-      {[+1, +5, +50, -1, -5, -50].map(element => (
-        <button onClick={modifyCounter}>sync {element}</button>
+      {[
+        { label: "+1", value: 1 },
+        { label: "+5", value: 5 },
+        { label: "-1", value: -1 },
+        { label: "-5", value: -5 },
+      ].map((element, id) => (
+        <button
+          key={id} // nupu id map funktsioonis
+          onClick={() => modifyCounter(element.value)}
+        >
+          sync {element.label}
+        </button>
       ))}
-
-      <button onClick={() => setTimeout(() => modifyCounter(), 2000)}>
-        async +1
-      </button>
     </>
   )
 }
