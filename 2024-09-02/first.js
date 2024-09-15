23761e6d0b8b61e4000fc9429b55185c9ef302af
Taavi <144431054+tv911473@users.noreply.github.com>
2024-09-15 11:41:18 +0300

    feat: added react-app and components

--- 2024-09-02/first.js
+++ 2024-09-02/first.js
@@ -1,13 +1,11 @@
 console.log(2 + 1);
 
-console.log(2 + '1');
+console.log(2 + "1");
 
 function addNumbers() {
-    console.log(2+1)
+  console.log(2 + 1);
 }
 
 function addNumbers(num1, num2) {
-    return num1 + num2
+  return num1 + num2;
 }
-
-
--- 2024-09-02/second.js
+++ 2024-09-02/second.js
@@ -1,55 +1,52 @@
-// luua array numbritega ja leida kindla numbri index
-const array = [1,2,3,4,5];
+// 1. luua array numbritega ja leida kindla numbri index
+const array = [1, 2, 3, 4, 5];
 
 function findIndex(array, num) {
-    return array.indexOf(num)
-};
+  return array.indexOf(num);
+}
 
 console.log(findIndex(array, 3));
 
-// liida 2 numbrit
+// 2. liida 2 numbrit
 function addNumbersFn(num1, num2) {
-    return num1 + num2
-};
+  return num1 + num2;
+}
 
 console.log(addNumbersFn(1, 2));
 
-// ymber kirjutada arrow funktsioonina
+// 3. ymber kirjutada arrow funktsioonina
 const addNumbersArrowFn = (num1, num2) => {
-    return num1 + num2
+  return num1 + num2;
 };
 
 console.log(addNumbersArrowFn(2, 2));
 
-// ymber kirjutada shorthand arrow funktsioonina
+// 4. ymber kirjutada shorthand arrow funktsioonina
 const addNumbersArrowFnShort = (num1, num2) => num1 + num2;
 
-
-// ylesanne
+// 5. ylesanne
 function addNumbersNested(num1) {
-    return function(num2) {
-        return num1 + num2
-    };
+  return function (num2) {
+    return num1 + num2;
+  };
 }
 console.log(addNumbersNested(3)(4));
 
-// eelmine func -> arrow func
-const addNumbersNestedArrowFn = (num1) => (num2) => num1 + num2
+// 6. eelmine func -> arrow func
+const addNumbersNestedArrowFn = (num1) => (num2) => num1 + num2;
 
-// liita igale elemendile juurde +5
+// 8. liita igale elemendile juurde +5
 const newArray = [1, 2, 3, 4, 5];
 
-const addedArray = newArray.map((element) => element +5)
+const addedArray = newArray.map((element) => element + 5);
 
 console.log(addedArray);
 
-// 3 parameetrit, mida saab map
+// 9. 3 parameetrit, mida saab map
 const threeParameters = newArray.map((element, index, array) => {
-    console.log(element, index, array)
+  console.log(element, index, array);
 
-    return element + 5;
-})
+  return element + 5;
+});
 
-console.log({threeParameters});
-
-// test for issue
+console.log({ threeParameters });
